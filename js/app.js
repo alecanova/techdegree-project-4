dd904adef183c0db37e2dfd8515120e642e82b93
Alessandro Canova <alessandrocanova14@gmail.com>
2020-06-16 23:30:43 +0200

    add click event to startGame

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+let game;
+
+document.getElementById('btn__reset').addEventListener('click', function() {
+
+    game = new Game();
+    game.startGame();
+
+});
