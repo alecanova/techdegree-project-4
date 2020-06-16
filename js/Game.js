1c0e69a33b563bc496b310b5ab479b302ee544c4
Alessandro Canova <alessandrocanova14@gmail.com>
2020-06-16 20:13:25 +0200

    Game and Phrase basics

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,54 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+
+    }
+
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases() {
+
+        const randomPhrases = [
+            new Phrase('Life is like a box of chocolates'),
+            new Phrase('There is no trying'),
+            new Phrase('May the force be with you'),
+            new Phrase('You have to see the matrix for yourself'),
+            new Phrase('You talking to me'),
+        ];
+
+        return randomPhrases;
+
+    }
+
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase() {
+
+        const randomIndex = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[randomIndex];
+
+    }
+
+    /**
+    * Begins game by selecting a random phrase and displaying it to user
+    */
+    startGame() {
+        const overlay = document.getElementById('overlay');
+        overlay.style.display = 'none';
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+       
+    }
+
+}
+
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,37 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+    * Display phrase on game board
+    */
+    addPhraseToDisplay() {
+        const ul = document.querySelector('#phrase ul');
+        let splitPhrase = this.phrase.split('');
+
+        for(let i = 0; i < this.phrase.length; i++) {
+
+            if(splitPhrase[i] === ' ') {
+                const space = document.createElement('li');
+                space.classList.add('space');
+                space.innerHTML = ' ';
+                ul.appendChild(space);
+
+            } else {
+
+                const letter = document.createElement('li');
+                letter.classList.add('letter');
+                letter.innerHTML = splitPhrase[i];
+                ul.appendChild(letter);
+                
+            }
+
+        }
+
+    }
+
+}
